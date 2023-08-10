f532c79acbe618862cea0b7f6eb36a07ef6daefe
Yura Basiuk <yurik.kamenev@gmail.com>
2023-08-10 20:53:44 +0300

    done task

--- contacts.js
+++ contacts.js
@@ -1,31 +1,45 @@
 const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-// contacts.js
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
-
-// TODO: задокументувати кожну функцію
-// function listContacts() {
-//   // ...твій код. Повертає масив контактів.
-// }
 const listContacts = async () => {
-  const contacts = await fs.readFile("./db/contacts.json", "utf-8");
-  console.log(contacts);
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 };
 
-listContacts();
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
